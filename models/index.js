975657b35d49d9301dae16430db85a36fd5ca54a
User <user@Users-MBP.hitronhub.home>
2020-08-19 20:01:05 -0400

    Add and Delete Players

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const playerSchema = new Schema({
-  name:String
+  name:String,
+  imgUrl:String
 })
 
 module.exports = mongoose.model('Player', playerSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const Player = require('../models/index');
+
 const request = require('request');
 const fetch = require('node-fetch')
 const jsdom = require('jsdom')
@@ -15,7 +17,6 @@ let playerDetails = [];
 let playerInfo_1 = null;
 let playerInfo_2 = null;
 let picURL = 'https://nba-players.herokuapp.com/players/'
-// let playername = 'kyle lowry'
 
 function get_nextSibling(n) {
     var y = n.nextSibling;
@@ -29,7 +30,6 @@ function get_nextSibling(n) {
 
 router.get('/', function(req,res, body){
   res.render('index')
-
 })
 
 router.get('/new',function(req,res){
@@ -81,4 +81,100 @@ router.get('/show', function(req,res){
   })
   // res.render('show')
 })
+
+router.get('/myteam', function(req,res){
+  Player.find({}, function(err, players) {
+    res.render('myteam', { players });
+  });
+})
+
+router.post('/myteam', function(req,res){
+  let player = new Player(req.body);
+  // player.name = req.query.player;
+  console.log(req.body)
+  player.save(function(err){
+    if(err){
+      console.log('error');
+      return;
+    }
+    res.redirect('/myteam')
+  })
+})
+
+
+router.get('/showMy', function(req,res){
+  let playername = req.query.player
+  fetch("https://www.fantasybasketballnerd.com/service/draft-projections")
+  .then(function(resp){
+    return resp.text();
+  })
+  .then(function(data){
+    let parser = new JSDOM(`${data}`,{contentType :'text/xml' });
+
+    console.log(playername)
+    let playerName = (parser.window.document.querySelectorAll('name'));
+    for(let i=0;i<playerName.length;i++){
+      if(playerName[i].innerHTML.toLowerCase() === playername){
+        console.log(playerName[i].innerHTML)
+        newPlayer = playerName[i].innerHTML
+        newPlayer_name = newPlayer.split(' ')
+        newPlayer_firstName = newPlayer_name[0];
+        newPlayer_lastName = newPlayer_name[1];
+        console.log(newPlayer_firstName, 'first name')
+        console.log(newPlayer_lastName, 'last name')
+        picURL = `${picURL}${newPlayer_lastName}/${newPlayer_firstName}`
+        console.log(picURL)
+        playerDetails.push(newPlayer)
+        playerInfo_1 = playerName[i]
+      }
+    }
+    for (let i = 0; i < 13; i++){
+      playerInfo_2 = get_nextSibling(playerInfo_1);
+      playerInfo_2_text = playerInfo_2.innerHTML
+      playerDetails.push(playerInfo_2_text)
+      playerInfo_1 = playerInfo_2
+    }
+    res.render('showMy', {
+      playerDetails:playerDetails,
+      playerPic:picURL
+    })
+    console.log(playerDetails)
+    picURL = 'https://nba-players.herokuapp.com/players/'
+    playerDetails = [];
+    return playerName;
+  })
+  console.log(req.query.player)
+})
+
+router.delete('/showMy/:playername', function(req,res){
+  let playerName = req.params.playername.replace('+',' ');
+  fetch("https://www.fantasybasketballnerd.com/service/draft-projections")
+  .then(function(resp){
+    return resp.text();
+  })
+  .then(function(data){
+    let parser = new JSDOM(`${data}`,{contentType :'text/xml' });
+    let playerName_ele = (parser.window.document.querySelectorAll('name'));
+    for(let i=0;i<playerName_ele.length;i++){
+      if(playerName_ele[i].innerHTML.toLowerCase() === playerName){
+        let playerName_delete = playerName_ele[i].innerHTML;
+        console.log(playerName_delete)
+        Player.deleteOne({name: playerName_delete}, function(err, result){
+          if(err){
+            console.log('error')
+          }
+          console.log(result)
+          // res.deletedCount
+          res.redirect('/myteam')
+        })
+      }
+    }
+  })
+
+
+
+  console.log(playerName)
+  //
+})
+
 module.exports = router;
